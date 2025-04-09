409758a4680d70b884a2f7e0a3f9dfc8ba8ec680
LucasAG1s <lucassaguilar500@gmail.com>
2025-04-09 11:46:48 -0300

    remove foooter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Footer } from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: "Ozonteck Products",
@@ -16,7 +15,6 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className="bg-white">
         {children}
-        <Footer />
       </body>
     </html>
   );
